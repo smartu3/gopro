91a792a9739f672cd5f606c0a4bcaadc6d1896f3
smartu3 <123389602@qq.com>
2019-07-28 10:45:54 +0800

    更新db相关信息

--- db.sql
+++ db.sql
@@ -1,6 +1,12 @@
-CREATE TABLE 'user' (
+SET FOREIGN_KEY_CHECKS=0;
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) DEFAULT '',
-  `age` int(11) DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4
+  `user_name` varchar(20) NOT NULL DEFAULT '',
+  `password` varchar(20) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `user` VALUES ('1', 'admin', '12164');
